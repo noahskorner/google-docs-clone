20b30b7516c4d35f471e63e8e2e406b1332b3ce5
Noah Korner <noahskorner@gmail.com>
2022-03-25 23:22:01 -0500

    Added status indicators.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
 import { Server } from 'socket.io';
+import jwt, { VerifyErrors } from 'jsonwebtoken';
 import http from 'http';
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 import app from './app';
 import env from './config/env.config';
+import documentService from './services/document.service';
 
 const server = http.createServer(app);
 
@@ -20,16 +22,54 @@ server.listen(env.PORT, () => {
 });
 
 io.on('connection', (socket) => {
-  const documentId = socket.handshake.query.documentId as string;
-  socket.join(documentId);
+  const accessToken = socket.handshake.query.accessToken as string | undefined;
+  const documentId = socket.handshake.query.documentId as string | undefined;
 
-  socket.on('send-changes', (rawDraftContentState) => {
-    socket.broadcast
-      .to(documentId)
-      .emit('receive-changes', rawDraftContentState);
-  });
+  if (accessToken === undefined || documentId === undefined)
+    return socket.disconnect();
+  else {
+    jwt.verify(
+      accessToken,
+      env.ACCESS_TOKEN_SECRET,
+      (err: VerifyErrors | null, decoded: unknown) => {
+        const { id, email } = decoded as RequestUser;
+        (socket as any).username = email;
 
-  socket.on('disconnect', () => {
-    socket.leave(documentId);
-  });
+        documentService
+          .findDocumentById(parseInt(documentId), parseInt(id))
+          .then(async (document) => {
+            if (document === null) return socket.disconnect();
+
+            socket.join(documentId);
+            io.in(documentId)
+              .fetchSockets()
+              .then((clients) => {
+                io.sockets.in(documentId).emit(
+                  'participant-update',
+                  clients.map((client) => (client as any).username)
+                );
+              });
+
+            socket.on('send-changes', (rawDraftContentState) => {
+              socket.broadcast
+                .to(documentId)
+                .emit('receive-changes', rawDraftContentState);
+            });
+
+            socket.on('disconnect', async () => {
+              socket.leave(documentId);
+              socket.disconnect();
+              io.in(documentId)
+                .fetchSockets()
+                .then((clients) => {
+                  io.sockets.in(documentId).emit(
+                    'participant-update',
+                    clients.map((client) => (client as any).username)
+                  );
+                });
+            });
+          });
+      }
+    );
+  }
 });
